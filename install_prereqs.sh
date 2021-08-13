2b9faf915d02410fd14e1204ad68a91e862caee1
Ivan Dovgan <ivandov@us.ibm.com>
2021-08-13 17:22:48 -0400

    feat: Move install steps to script for external use

---
+++ install_prereqs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# TODOs:
+# - Make cross-arch/platform compatible (at least OSX and linux)
+# - Revert to using the official conventional-changelog plugin when PR is merged
+
+npm install -g release-it
+# Fork until this PR is merged: https://github.com/release-it/conventional-changelog/pull/27
+npm install -g https://github.com/ivandov/conventional-changelog
+pip install ansible-core
+wget "https://github.com/mikefarah/yq/releases/download/v4.9.6/yq_linux_amd64" -O /usr/bin/yq
+chmod +x /usr/bin/yq
