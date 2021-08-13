bc189ea0aa16f04b5a4e8c4febb220111aa0c2ab
Ivan Dovgan <ivandov@us.ibm.com>
2021-08-13 17:56:30 -0400

    ci: moved git HEAD fix to setup script

--- scripts/setup_travis.sh
+++ scripts/setup_travis.sh
@@ -4,9 +4,16 @@
 # - Make cross-arch/platform compatible (at least OSX and linux)
 # - Revert to using the official conventional-changelog plugin when PR is merged
 
+
+echo "Installing release-it and oc-releaser pre-reqs"
 npm install -g release-it
 # Fork until this PR is merged: https://github.com/release-it/conventional-changelog/pull/27
 npm install -g https://github.com/ivandov/conventional-changelog
 pip install ansible-core
 sudo wget "https://github.com/mikefarah/yq/releases/download/v4.9.6/yq_linux_amd64" -O /usr/bin/yq
 sudo chmod +x /usr/bin/yq
+
+echo "Running in a Travis environment, re-attaching git HEAD to ${TRAVIS_BRANCH}"
+git remote rm origin
+git remote add origin https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+git symbolic-ref HEAD "refs/heads/${TRAVIS_BRANCH}"
