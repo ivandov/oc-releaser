99b46f6fc07c96cf1a708ee56a5b49f98c987b1b
Ivan Dovgan <ivandov@us.ibm.com>
2022-02-14 17:20:00 -0500

    Update setup_travis.sh

--- scripts/setup_travis.sh
+++ scripts/setup_travis.sh
@@ -7,8 +7,7 @@
 
 echo "Installing release-it and oc-releaser pre-reqs"
 npm install -g release-it
-# Fork until this PR is merged: https://github.com/release-it/conventional-changelog/pull/27
-npm install -g https://github.com/ivandov/conventional-changelog
+npm install -g @release-it/conventional-changelog
 pip install ansible-core
 sudo wget "https://github.com/mikefarah/yq/releases/download/v4.9.6/yq_linux_amd64" -O /usr/bin/yq
 sudo chmod +x /usr/bin/yq
